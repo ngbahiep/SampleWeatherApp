2308a5e3447e4e7b3ff6310936f9f740811500ad
Nguyen Ba Hiep <ngbahiep90@gmail.com>
2020-06-13 11:35:48 +0800

    - Add search bar into navigation item

--- SampleWeatherApp/ViewController.swift
+++ SampleWeatherApp/ViewController.swift
@@ -13,28 +13,67 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    let searchController = UISearchController(searchResultsController: nil)
+    
     var weather: Weather?
+    var request: Request?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // configure UISearchController
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        
+        // Add search controller to navigation item
+        navigationItem.searchController = searchController
+        
+        // configure UITableView
         tableView.dataSource = self
         tableView.delegate = self
-        
+    }
+    
+    func searchWeather(city: String, completion: @escaping (Weather?) -> Void) {
+        print("searchWeather: \(city)")
         if NetworkReachabilityManager()?.isReachable ?? false {
             print("Got internet connection")
-            AF.request("https://api.openweathermap.org/data/2.5/forecast/daily?q=saigon&cnt=7&appid=60c6fbeb4b93ac653c492ba806fc346d&units=metric").responseDecodable( of: Weather.self) { response in
+            
+            let strUrl = "https://api.openweathermap.org/data/2.5/forecast/daily?q=\(city)&cnt=7&appid=60c6fbeb4b93ac653c492ba806fc346d&units=metric"
+            
+            print("strURL: \(strUrl)")
+            
+            guard let url = URL(string: strUrl) else { return }
+            var req = URLRequest(url: url)
+            req.cachePolicy = NSURLRequest.CachePolicy.useProtocolCachePolicy
+            
+            // Invalidate previous request
+            if let request = request {
+                request.cancel()
+            }
+            
+            request = AF.request(req)
+                .validate()
+                .responseDecodable( of: Weather.self) { response in
                 
-//                print(response.description)
+                print(response.description)
                 
-                guard let weather = response.value else { return }
-                self.weather = weather
+                if (nil != response.error) {
+                    print("ERROR: \(String(describing: response.error))")
+                    completion(nil)
+                    return
+                }
                 
-                self.tableView.reloadData()
+                guard let weather = response.value else {
+                    completion(nil)
+                    return
+                }
+                    
+                completion(weather)
                 
                 print(weather)
                 
             }
+            
         } else {
             print("No internet connection")
         }
@@ -63,4 +102,26 @@ extension ViewController: UITableViewDataSource {
         return cell
     }
     
+}
+
+extension ViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let strSearch = searchController.searchBar.text else {
+            return
+        }
+        
+        if strSearch.count >= 3 {
+            // start progress
+            Prog.start(in: self.tableView, .activityIndicator)
+            
+            searchWeather(city: strSearch) { (weather) in
+                print("Reload data ...")
+                self.weather = weather
+                self.tableView.reloadData()
+                
+                // dismiss progress
+                Prog.dismiss(in: self.tableView)
+            }   
+        }
+    }
 }
